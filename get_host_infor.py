e904f97f5b4b95872bf799731d37dc1ff8d103bf
doctor305 <94644276@qq.com>
2016-04-26 16:01:18 +0800

    Update.

--- get_host_infor.py
+++ get_host_infor.py
@@ -1,13 +1,22 @@
 import socket
 
-def get_host_info():
+def get_self_info():
     host_name = socket.gethostname()
-
     print "Host name is %s" % host_name
 
     IP_address = socket.gethostbyname(host_name)
-
     print "IP address is %s" % IP_address
+    return host_name,IP_address
+
+def remote_machine_info(hostname):
+    try:
+        print "%s IP addres is %s" % (hostname,socket.gethostbyname(hostname))
+    except socket.error, err_msg:
+        print "%s: %s" % (hostname,err_msg)
+    
 
 if __name__ == "__main__":
-    get_host_info()
+    get_self_info()
+
+    remote_machine_info("www.baidu.com")
+    remote_machine_info("abc")
