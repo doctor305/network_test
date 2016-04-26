3cf563bce14801488c08099c95798f4b7d2d8251
doctor305 <94644276@qq.com>
2016-04-26 15:38:28 +0800

    Update.

--- get_host_infor.py
+++ get_host_infor.py
@@ -1,9 +1,13 @@
 import socket
 
-host_name = socket.gethostname()
+def get_host_info():
+    host_name = socket.gethostname()
 
-print 'Host name is %s' % host_name
+    print "Host name is %s" % host_name
 
-IP_address = socket.gethostbyname(host_name)
+    IP_address = socket.gethostbyname(host_name)
 
-print 'IP address is %s' % IP_address
+    print "IP address is %s" % IP_address
+
+if __name__ == "__main__":
+    get_host_info()
